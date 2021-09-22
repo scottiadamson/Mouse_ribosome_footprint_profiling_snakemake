6e3f4fa658f5f23edd2f51905fb0356ffec5f304
scottiadamson <scott.i.adamson@gmail.com>
2021-09-22 15:33:02 -0400

    Fix rRNA references

--- scripts/combine_ncRNA_tRNAs.py
+++ scripts/combine_ncRNA_tRNAs.py
@@ -1,25 +1,28 @@
 import gzip
 
-tRNA_lines = ''
-a = open('mm10-mature-tRNAs.fa', 'r')
-for line in a:
-    tRNA_lines += line
-a.close()
-a = gzip.open('GCF_000001635.27_GRCm39_rna.fna.gz', 'rt')
+#tRNA_lines = ''
+#a = open('mm10-mature-tRNAs.fa', 'r')
+#for line in a:
+#    tRNA_lines += line
+#a.close()
+
 b = open('references/rRNA_snoRNA_tRNA.fa', 'w')
-for line in a:
-    if line[0] == '>':
-        if 'NR' in line:
-            if 'ribosomal RNA' in line and 'non-coding RNA' not in line:
-                write = True
-            elif 'small nucleolar RNA' in line and 'long non-coding RNA' not in line:
-                write = True
+for rna_ref in ['1','2']:
+    a = gzip.open('mouse.' + rna_ref + '.rna.fna.gz', 'rt')
+    for line in a:
+        if line[0] == '>':
+            if 'NR' in line:
+                if 'ribosomal RNA' in line and 'non-coding RNA' not in line:
+                    write = True
+                elif 'small nucleolar RNA' in line and 'long non-coding RNA' not in line:
+                    write = True
+                else:
+                    write = False
             else:
                 write = False
-        else:
-            write = False
-    if write:
-        b.write(line)
+        if write:
+            b.write(line)
+    a.close()
 b.write(tRNA_lines)
-a.close();b.close()
+b.close()
 
